560bcdf8907110feba2e29961299748c57e3b310
Muuminuu <cedric.frachisse@gmail.com>
2025-02-18 14:52:34 +0100

    add(Task): create Task Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at'
     ];
 
     /**
@@ -45,4 +46,5 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
 }
